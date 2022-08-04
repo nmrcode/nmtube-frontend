b332d2815ac76b444412fd1e634d165cd38333e7
nmcode <nmcode.personal@gmail.com>
2022-08-04 23:17:09 +0300

    подключение react-hook-form

--- core/components/ui/form/field/Field.tsx
+++ core/components/ui/form/field/Field.tsx
@@ -1,23 +1,43 @@
-import { FC, HTMLAttributes } from "react";
+import { FC, forwardRef, HTMLAttributes } from "react";
 import clsx from "clsx";
 
 import s from "./Field.module.scss";
 
-interface IFieldProps {
+import { ButtonHTMLAttributes, InputHTMLAttributes } from "react";
+import { FieldError } from "react-hook-form";
+
+export interface IButton extends ButtonHTMLAttributes<HTMLButtonElement> {}
+
+export interface IFieldProps {
+  placeholder: string;
+  error?: FieldError | undefined;
   icon?: JSX.Element;
 }
 
-const Field: FC<HTMLAttributes<HTMLInputElement> & IFieldProps> = ({
-  className,
-  icon,
-  ...rest
-}) => {
-  return (
-    <div className={s.wrapper}>
-      {icon && <div className={s.icon}>{icon}</div>}
-      <input className={clsx(className, s.input)} {...rest} />
-    </div>
-  );
-};
+type TypeInputPropsField = InputHTMLAttributes<HTMLInputElement> & IFieldProps;
+
+export interface IField extends TypeInputPropsField {}
+
+const Field = forwardRef<HTMLInputElement, IField>(
+  ({ error, icon, placeholder, type = "text", style, ...rest }, ref) => {
+    return (
+      <div className={clsx(s.common, s.wrapper)} style={style}>
+        <label>
+          <div className={s.icon}>{icon}</div>
+          <input
+            type={type}
+            ref={ref}
+            {...rest}
+            className={s.input}
+            placeholder={placeholder}
+          />
+        </label>
+        {error && <div className={s.error}>{error.message}</div>}
+      </div>
+    );
+  }
+);
+
+Field.displayName = "Field";
 
 export default Field;
--- core/screens/auth/AuthScreen.tsx
+++ core/screens/auth/AuthScreen.tsx
@@ -1,20 +1,44 @@
-import { FC } from "react";
+import { EventHandler, FC, FormEvent } from "react";
 import s from "./AuthScreen.module.scss";
 import { observer } from "mobx-react-lite";
 import Field from "@/components/ui/form/field/Field";
 import Button from "@/components/ui/button/Button";
 import Link from "next/link";
+import { SubmitHandler, useForm } from "react-hook-form";
+
+type Inputs = {
+  email: string;
+  password: string;
+};
 
 const AuthScreen: FC = observer(() => {
+  const {
+    register,
+    handleSubmit,
+    watch,
+    reset,
+    formState: { errors },
+  } = useForm<Inputs>();
+
+  const onLogin = (e: FormEvent<HTMLButtonElement>) => {
+    e.preventDefault();
+    reset();
+  };
+
+  const onRegister = (e: FormEvent<HTMLButtonElement>) => {
+    e.preventDefault();
+    reset();
+  };
+
   return (
     <div className={s.wrapper}>
       <h1 className={s.title}>Войдите или зарегистрируйтесь</h1>
       <form className={s.form}>
-        <Field placeholder="Email ..." />
-        <Field placeholder="Password ..." />
+        <Field {...register("email")} placeholder="Email ..." />
+        <Field {...register("password")} placeholder="Password ..." />
         <div className={s.buttons}>
-          <Button>Войти</Button>
-          <Button>Зарегистрироваться</Button>
+          <Button onClick={onLogin}>Войти</Button>
+          <Button onClick={onRegister}>Зарегистрироваться</Button>
         </div>
         <Link href="/">
           <a className={s.back}>Назад к сайту</a>
